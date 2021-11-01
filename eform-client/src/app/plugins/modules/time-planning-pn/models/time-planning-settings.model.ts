a88eac27243f22fb37be54bac7d02dd7943d718d
Alex <sahok6264@gmail.com>
2021-11-01 19:35:43 +0200

    Delete not need props

--- eform-client/src/app/plugins/modules/time-planning-pn/models/time-planning-settings.model.ts
+++ eform-client/src/app/plugins/modules/time-planning-pn/models/time-planning-settings.model.ts
@@ -1,8 +1,5 @@
 export class TimePlanningSettingsModel {
   folderId?: number;
   eformId?: number;
-  folderTasksId?: number;
-  folderName: string;
-  folderTasksName: string;
   assignedSites: number[] = [];
 }
