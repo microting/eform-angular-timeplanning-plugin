150029be2691f58ac6a06e648f52b164debdc255
René Schultz Madsen <rm@microting.com>
2021-11-11 15:07:11 +0000

    Fixing the dev scripts.

--- devgetchanges.sh
+++ devgetchanges.sh
@@ -2,22 +2,22 @@
 
 cd ~
 
-rm -fR Documents/workspace/microting/eform-time-planning-plugin/eform-client/src/app/plugins/modules/time-planning-pn
+rm -fR Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/src/app/plugins/modules/time-planning-pn
 
-cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/time-planning-pn Documents/workspace/microting/eform-time-planning-plugin/eform-client/src/app/plugins/modules/time-planning-pn
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/time-planning-pn Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/src/app/plugins/modules/time-planning-pn
 
-rm -fR Documents/workspace/microting/eform-time-planning-plugin/eFormAPI/Plugins/TimePlanning.Pn
+rm -fR Documents/workspace/microting/eform-angular-timeplanning-plugin/eFormAPI/Plugins/TimePlanning.Pn
 
-cp -a Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/TimePlanning.Pn Documents/workspace/microting/eform-time-planning-plugin/eFormAPI/Plugins/TimePlanning.Pn
+cp -a Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/TimePlanning.Pn Documents/workspace/microting/eform-angular-timeplanning-plugin/eFormAPI/Plugins/TimePlanning.Pn
 
 # Test files rm
-rm -fR Documents/workspace/microting/eform-time-planning-plugin/eform-client/e2e/Tests/time-planning-settings/
-rm -fR Documents/workspace/microting/eform-time-planning-plugin/eform-client/e2e/Tests/time-planning-general/
-rm -fR Documents/workspace/microting/eform-time-planning-plugin/eform-client/wdio-headless-plugin-step2.conf.js
-rm -fR Documents/workspace/microting/eform-time-planning-plugin/eform-client/e2e/Page\ objects/TimePlanning
+rm -fR Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/e2e/Tests/time-planning-settings/
+rm -fR Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/e2e/Tests/time-planning-general/
+rm -fR Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/wdio-headless-plugin-step2.conf.js
+rm -fR Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/e2e/Page\ objects/TimePlanning
 
 # Test files cp
-cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/time-planning-settings Documents/workspace/microting/eform-time-planning-plugin/eform-client/e2e/Tests/time-planning-settings
-cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/time-planning-general Documents/workspace/microting/eform-time-planning-plugin/eform-client/e2e/Tests/time-planning-general
-cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js Documents/workspace/microting/eform-time-planning-plugin/eform-client/wdio-headless-plugin-step2.conf.js
-cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/TimePlanning Documents/workspace/microting/eform-time-planning-plugin/eform-client/e2e/Page\ objects/TimePlanning
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/time-planning-settings Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/e2e/Tests/time-planning-settings
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/time-planning-general Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/e2e/Tests/time-planning-general
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/wdio-headless-plugin-step2.conf.js
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/TimePlanning Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/e2e/Page\ objects/TimePlanning
