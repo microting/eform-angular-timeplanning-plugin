93b42dc04369d4d471390504a94747f1fbf522fb
René Schultz Madsen <rm@microting.com>
2023-05-11 07:35:55 +0200

    Adding changes to support the new parallel testing structure.

--- devinstall.sh
+++ devinstall.sh
@@ -16,7 +16,7 @@ cp -a Documents/workspace/microting/eform-angular-timeplanning-plugin/eFormAPI/P
 rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/time-planning-settings
 rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/time-planning-general
 rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/TimePlanning
-rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.ts
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-headless-plugin-step2a.conf.ts
 mkdir -p Documents/workspace/microting/eform-angular-frontend/eform-client/cypress/e2e/plugins
 rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/cypress/e2e/plugins/time-planning-pn
 
@@ -24,5 +24,5 @@ rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/cypress
 cp -a Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/e2e/Tests/time-planning-settings Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/time-planning-settings
 cp -a Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/e2e/Tests/time-planning-general Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/time-planning-general
 cp -a Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/e2e/Page\ objects/TimePlanning Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/TimePlanning
-cp -a Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/wdio-headless-plugin-step2.conf.ts Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.ts
+cp -a Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/wdio-headless-plugin-step2.conf.ts Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-headless-plugin-step2a.conf.ts
 cp -a Documents/workspace/microting/eform-angular-timeplanning-plugin/eform-client/cypress/e2e/plugins/time-planning-pn Documents/workspace/microting/eform-angular-frontend/eform-client/cypress/e2e/plugins/time-planning-pn
