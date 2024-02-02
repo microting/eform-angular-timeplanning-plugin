7eb442cb6ae19d6358b83bb8f2161ae33926cad7
Alex Horban <sahok6264@gmail.com>
2024-02-02 19:42:40 +0200

    Fix translations plugin issue
    fixed if the user leave from plugin pages and then returned again - plugin translations were uploaded again

--- eform-client/src/app/plugins/modules/time-planning-pn/layouts/time-planning-pn-layout.component.ts
+++ eform-client/src/app/plugins/modules/time-planning-pn/layouts/time-planning-pn-layout.component.ts
@@ -1,24 +1,38 @@
 import {AfterContentInit, Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {translates} from './../i18n/translates';
+import {Store} from '@ngrx/store';
+import {addPluginToVisited, selectPluginsVisitedPlugins} from 'src/app/state';
+import {take} from 'rxjs';
 
 @Component({
   selector: 'app-time-planning-pn-layout',
   template: `
     <router-outlet></router-outlet>`,
 })
-export class TimePlanningPnLayoutComponent
-  implements AfterContentInit, OnInit {
+export class TimePlanningPnLayoutComponent implements AfterContentInit, OnInit {
+  private pluginName = 'time-planning';
 
   constructor(
     private translateService: TranslateService,
+    store: Store
   ) {
+    store.select(selectPluginsVisitedPlugins)
+      .pipe(take(1))
+      .subscribe(x => {
+        // check current plugin in activated plugin
+        if (x.findIndex(y => y === this.pluginName) === -1) {
+          // add all plugin translates one time
+          Object.keys(translates).forEach(locale => {
+            this.translateService.setTranslation(locale, translates[locale], true);
+          });
+          // add plugin to visited plugins
+          store.dispatch(addPluginToVisited(this.pluginName));
+        }
+      });
   }
 
   ngOnInit() {
-    Object.keys(translates).forEach(locale => {
-      this.translateService.setTranslation(locale, translates[locale], true);
-    });
   }
 
   ngAfterContentInit() {
