1299efc37dae24f0d422ee89a51c08a1f650035a
René Schultz Madsen <rm@microting.dk>
2025-03-05 07:05:30 +0100

    Adding changes to set plannings for the future.

--- eFormAPI/Plugins/TimePlanning.Pn/TimePlanning.Pn/Services/TimePlanningSettingService/TimeSettingService.cs
+++ eFormAPI/Plugins/TimePlanning.Pn/TimePlanning.Pn/Services/TimePlanningSettingService/TimeSettingService.cs
@@ -531,6 +531,191 @@ public class TimeSettingService : ISettingService
 
         await dbAssignedSite.Update(_dbContext);
 
+        if (!dbAssignedSite.UseGoogleSheetAsDefault)
+        {
+            var midnight = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, 0, 0, 0);
+            var planRegistrationsFromTodayAndForward = await _dbContext.PlanRegistrations
+                .Where(x => x.WorkflowState != Constants.WorkflowStates.Removed)
+                .Where(x => x.SdkSitId == siteId)
+                .Where(x => x.Date >= midnight)
+                .ToListAsync();
+
+            if (planRegistrationsFromTodayAndForward.Count == 0)
+            {
+                // create new plannings for 30 days as of today and forward
+                for (int i = 0; i < 30; i++)
+                {
+                    var newPlanRegistration = new PlanRegistration
+                    {
+                        Date = midnight.AddDays(i),
+                        SdkSitId = siteId,
+                        CreatedByUserId = _userService.UserId,
+                        UpdatedByUserId = _userService.UserId
+                    };
+
+                    await newPlanRegistration.Create(_dbContext);
+                }
+            }
+
+            if (planRegistrationsFromTodayAndForward.Count < 30)
+            {
+                var lastDate = planRegistrationsFromTodayAndForward.Max(x => x.Date);
+                for (int i = 1; i < planRegistrationsFromTodayAndForward.Count; i++)
+                {
+                    var newPlanRegistration = new PlanRegistration
+                    {
+                        Date = lastDate.AddDays(i),
+                        SdkSitId = siteId,
+                        CreatedByUserId = _userService.UserId,
+                        UpdatedByUserId = _userService.UserId
+                    };
+
+                    await newPlanRegistration.Create(_dbContext);
+                }
+            }
+
+            planRegistrationsFromTodayAndForward = await _dbContext.PlanRegistrations
+                .Where(x => x.WorkflowState != Constants.WorkflowStates.Removed)
+                .Where(x => x.SdkSitId == siteId)
+                .Where(x => x.Date >= midnight)
+                .ToListAsync();
+
+            foreach (var planRegistration in planRegistrationsFromTodayAndForward)
+            {
+                var dayOfWeek = planRegistration.Date.DayOfWeek;
+                var innerMidnight = new DateTime(planRegistration.Date.Year, planRegistration.Date.Month, planRegistration.Date.Day, 0, 0, 0);
+
+                //var dayOfWeek = newPlanRegistration.Date.DayOfWeek;
+
+                    switch (dayOfWeek)
+                    {
+                        case DayOfWeek.Monday:
+                            planRegistration.PlannedStartOfShift1 = dbAssignedSite.StartMonday ?? 0;
+                            planRegistration.PlannedEndOfShift1 = dbAssignedSite.EndMonday ?? 0;
+                            planRegistration.PlannedBreakOfShift1 = dbAssignedSite.BreakMonday ?? 0;
+                            planRegistration.PlannedStartOfShift2 = dbAssignedSite.StartMonday2NdShift ?? 0;
+                            planRegistration.PlannedEndOfShift2 = dbAssignedSite.EndMonday2NdShift ?? 0;
+                            planRegistration.PlannedBreakOfShift2 = dbAssignedSite.BreakMonday2NdShift ?? 0;
+                            planRegistration.PlannedStartOfShift3 = dbAssignedSite.StartMonday3RdShift ?? 0;
+                            planRegistration.PlannedEndOfShift3 = dbAssignedSite.EndMonday3RdShift ?? 0;
+                            planRegistration.PlannedBreakOfShift3 = dbAssignedSite.BreakMonday3RdShift ?? 0;
+                            planRegistration.PlannedStartOfShift4 = dbAssignedSite.StartMonday4ThShift ?? 0;
+                            planRegistration.PlannedEndOfShift4 = dbAssignedSite.EndMonday4ThShift ?? 0;
+                            planRegistration.PlannedBreakOfShift4 = dbAssignedSite.BreakMonday4ThShift ?? 0;
+                            planRegistration.PlannedStartOfShift5 = dbAssignedSite.StartMonday5ThShift ?? 0;
+                            planRegistration.PlannedEndOfShift5 = dbAssignedSite.EndMonday5ThShift ?? 0;
+                            planRegistration.PlannedBreakOfShift5 = dbAssignedSite.BreakMonday5ThShift ?? 0;
+                            break;
+                        case DayOfWeek.Tuesday:
+                            planRegistration.PlannedStartOfShift1 = dbAssignedSite.StartTuesday ?? 0;
+                            planRegistration.PlannedEndOfShift1 = dbAssignedSite.EndTuesday ?? 0;
+                            planRegistration.PlannedBreakOfShift1 = dbAssignedSite.BreakTuesday ?? 0;
+                            planRegistration.PlannedStartOfShift2 = dbAssignedSite.StartTuesday2NdShift ?? 0;
+                            planRegistration.PlannedEndOfShift2 = dbAssignedSite.EndTuesday2NdShift ?? 0;
+                            planRegistration.PlannedBreakOfShift2 = dbAssignedSite.BreakTuesday2NdShift ?? 0;
+                            planRegistration.PlannedStartOfShift3 = dbAssignedSite.StartTuesday3RdShift ?? 0;
+                            planRegistration.PlannedEndOfShift3 = dbAssignedSite.EndTuesday3RdShift ?? 0;
+                            planRegistration.PlannedBreakOfShift3 = dbAssignedSite.BreakTuesday3RdShift ?? 0;
+                            planRegistration.PlannedStartOfShift4 = dbAssignedSite.StartTuesday4ThShift ?? 0;
+                            planRegistration.PlannedEndOfShift4 = dbAssignedSite.EndTuesday4ThShift ?? 0;
+                            planRegistration.PlannedBreakOfShift4 = dbAssignedSite.BreakTuesday4ThShift ?? 0;
+                            planRegistration.PlannedStartOfShift5 = dbAssignedSite.StartTuesday5ThShift ?? 0;
+                            planRegistration.PlannedEndOfShift5 = dbAssignedSite.EndTuesday5ThShift ?? 0;
+                            planRegistration.PlannedBreakOfShift5 = dbAssignedSite.BreakTuesday5ThShift ?? 0;
+                            break;
+                        case DayOfWeek.Wednesday:
+                            planRegistration.PlannedStartOfShift1 = dbAssignedSite.StartWednesday ?? 0;
+                            planRegistration.PlannedEndOfShift1 = dbAssignedSite.EndWednesday ?? 0;
+                            planRegistration.PlannedBreakOfShift1 = dbAssignedSite.BreakWednesday ?? 0;
+                            planRegistration.PlannedStartOfShift2 = dbAssignedSite.StartWednesday2NdShift ?? 0;
+                            planRegistration.PlannedEndOfShift2 = dbAssignedSite.EndWednesday2NdShift ?? 0;
+                            planRegistration.PlannedBreakOfShift2 = dbAssignedSite.BreakWednesday2NdShift ?? 0;
+                            planRegistration.PlannedStartOfShift3 = dbAssignedSite.StartWednesday3RdShift ?? 0;
+                            planRegistration.PlannedEndOfShift3 = dbAssignedSite.EndWednesday3RdShift ?? 0;
+                            planRegistration.PlannedBreakOfShift3 = dbAssignedSite.BreakWednesday3RdShift ?? 0;
+                            planRegistration.PlannedStartOfShift4 = dbAssignedSite.StartWednesday4ThShift ?? 0;
+                            planRegistration.PlannedEndOfShift4 = dbAssignedSite.EndWednesday4ThShift ?? 0;
+                            planRegistration.PlannedBreakOfShift4 = dbAssignedSite.BreakWednesday4ThShift ?? 0;
+                            planRegistration.PlannedStartOfShift5 = dbAssignedSite.StartWednesday5ThShift ?? 0;
+                            planRegistration.PlannedEndOfShift5 = dbAssignedSite.EndWednesday5ThShift ?? 0;
+                            planRegistration.PlannedBreakOfShift5 = dbAssignedSite.BreakWednesday5ThShift ?? 0;
+                            break;
+                        case DayOfWeek.Thursday:
+                            planRegistration.PlannedStartOfShift1 = dbAssignedSite.StartThursday ?? 0;
+                            planRegistration.PlannedEndOfShift1 = dbAssignedSite.EndThursday ?? 0;
+                            planRegistration.PlannedBreakOfShift1 = dbAssignedSite.BreakThursday ?? 0;
+                            planRegistration.PlannedStartOfShift2 = dbAssignedSite.StartThursday2NdShift ?? 0;
+                            planRegistration.PlannedEndOfShift2 = dbAssignedSite.EndThursday2NdShift ?? 0;
+                            planRegistration.PlannedBreakOfShift2 = dbAssignedSite.BreakThursday2NdShift ?? 0;
+                            planRegistration.PlannedStartOfShift3 = dbAssignedSite.StartThursday3RdShift ?? 0;
+                            planRegistration.PlannedEndOfShift3 = dbAssignedSite.EndThursday3RdShift ?? 0;
+                            planRegistration.PlannedBreakOfShift3 = dbAssignedSite.BreakThursday3RdShift ?? 0;
+                            planRegistration.PlannedStartOfShift4 = dbAssignedSite.StartThursday4ThShift ?? 0;
+                            planRegistration.PlannedEndOfShift4 = dbAssignedSite.EndThursday4ThShift ?? 0;
+                            planRegistration.PlannedBreakOfShift4 = dbAssignedSite.BreakThursday4ThShift ?? 0;
+                            planRegistration.PlannedStartOfShift5 = dbAssignedSite.StartThursday5ThShift ?? 0;
+                            planRegistration.PlannedEndOfShift5 = dbAssignedSite.EndThursday5ThShift ?? 0;
+                            planRegistration.PlannedBreakOfShift5 = dbAssignedSite.BreakThursday5ThShift ?? 0;
+                            break;
+                        case DayOfWeek.Friday:
+                            planRegistration.PlannedStartOfShift1 = dbAssignedSite.StartFriday ?? 0;
+                            planRegistration.PlannedEndOfShift1 = dbAssignedSite.EndFriday ?? 0;
+                            planRegistration.PlannedBreakOfShift1 = dbAssignedSite.BreakFriday ?? 0;
+                            planRegistration.PlannedStartOfShift2 = dbAssignedSite.StartFriday2NdShift ?? 0;
+                            planRegistration.PlannedEndOfShift2 = dbAssignedSite.EndFriday2NdShift ?? 0;
+                            planRegistration.PlannedBreakOfShift2 = dbAssignedSite.BreakFriday2NdShift ?? 0;
+                            planRegistration.PlannedStartOfShift3 = dbAssignedSite.StartFriday3RdShift ?? 0;
+                            planRegistration.PlannedEndOfShift3 = dbAssignedSite.EndFriday3RdShift ?? 0;
+                            planRegistration.PlannedBreakOfShift3 = dbAssignedSite.BreakFriday3RdShift ?? 0;
+                            planRegistration.PlannedStartOfShift4 = dbAssignedSite.StartFriday4ThShift ?? 0;
+                            planRegistration.PlannedEndOfShift4 = dbAssignedSite.EndFriday4ThShift ?? 0;
+                            planRegistration.PlannedBreakOfShift4 = dbAssignedSite.BreakFriday4ThShift ?? 0;
+                            planRegistration.PlannedStartOfShift5 = dbAssignedSite.StartFriday5ThShift ?? 0;
+                            planRegistration.PlannedEndOfShift5 = dbAssignedSite.EndFriday5ThShift ?? 0;
+                            planRegistration.PlannedBreakOfShift5 = dbAssignedSite.BreakFriday5ThShift ?? 0;
+                            break;
+                        case DayOfWeek.Saturday:
+                            planRegistration.PlannedStartOfShift1 = dbAssignedSite.StartSaturday ?? 0;
+                            planRegistration.PlannedEndOfShift1 = dbAssignedSite.EndSaturday ?? 0;
+                            planRegistration.PlannedBreakOfShift1 = dbAssignedSite.BreakSaturday ?? 0;
+                            planRegistration.PlannedStartOfShift2 = dbAssignedSite.StartSaturday2NdShift ?? 0;
+                            planRegistration.PlannedEndOfShift2 = dbAssignedSite.EndSaturday2NdShift ?? 0;
+                            planRegistration.PlannedBreakOfShift2 = dbAssignedSite.BreakSaturday2NdShift ?? 0;
+                            planRegistration.PlannedStartOfShift3 = dbAssignedSite.StartSaturday3RdShift ?? 0;
+                            planRegistration.PlannedEndOfShift3 = dbAssignedSite.EndSaturday3RdShift ?? 0;
+                            planRegistration.PlannedBreakOfShift3 = dbAssignedSite.BreakSaturday3RdShift ?? 0;
+                            planRegistration.PlannedStartOfShift4 = dbAssignedSite.StartSaturday4ThShift ?? 0;
+                            planRegistration.PlannedEndOfShift4 = dbAssignedSite.EndSaturday4ThShift ?? 0;
+                            planRegistration.PlannedBreakOfShift4 = dbAssignedSite.BreakSaturday4ThShift ?? 0;
+                            planRegistration.PlannedStartOfShift5 = dbAssignedSite.StartSaturday5ThShift ?? 0;
+                            planRegistration.PlannedEndOfShift5 = dbAssignedSite.EndSaturday5ThShift ?? 0;
+                            planRegistration.PlannedBreakOfShift5 = dbAssignedSite.BreakSaturday5ThShift ?? 0;
+                            break;
+                        case DayOfWeek.Sunday:
+                            planRegistration.PlannedStartOfShift1 = dbAssignedSite.StartSunday ?? 0;
+                            planRegistration.PlannedEndOfShift1 = dbAssignedSite.EndSunday ?? 0;
+                            planRegistration.PlannedBreakOfShift1 = dbAssignedSite.BreakSunday ?? 0;
+                            planRegistration.PlannedStartOfShift2 = dbAssignedSite.StartSunday2NdShift ?? 0;
+                            planRegistration.PlannedEndOfShift2 = dbAssignedSite.EndSunday2NdShift ?? 0;
+                            planRegistration.PlannedBreakOfShift2 = dbAssignedSite.BreakSunday2NdShift ?? 0;
+                            planRegistration.PlannedStartOfShift3 = dbAssignedSite.StartSunday3RdShift ?? 0;
+                            planRegistration.PlannedEndOfShift3 = dbAssignedSite.EndSunday3RdShift ?? 0;
+                            planRegistration.PlannedBreakOfShift3 = dbAssignedSite.BreakSunday3RdShift ?? 0;
+                            planRegistration.PlannedStartOfShift4 = dbAssignedSite.StartSunday4ThShift ?? 0;
+                            planRegistration.PlannedEndOfShift4 = dbAssignedSite.EndSunday4ThShift ?? 0;
+                            planRegistration.PlannedBreakOfShift4 = dbAssignedSite.BreakSunday4ThShift ?? 0;
+                            planRegistration.PlannedStartOfShift5 = dbAssignedSite.StartSunday5ThShift ?? 0;
+                            planRegistration.PlannedEndOfShift5 = dbAssignedSite.EndSunday5ThShift ?? 0;
+                            planRegistration.PlannedBreakOfShift5 = dbAssignedSite.BreakSunday5ThShift ?? 0;
+                            break;
+                    }
+
+                    await planRegistration.Update(_dbContext);
+
+            }
+
+        }
+
         return new OperationResult(true, _localizationService.GetString("AssignedSiteUpdatedSuccessfuly"));
     }
 }
