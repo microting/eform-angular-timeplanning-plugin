a9cdad902c20072fbe554074676a6449bb4722bb
René Schultz Madsen <rm@microting.dk>
2025-04-01 06:42:03 +0200

    Adding the new timepicker.

--- eform-client/src/app/plugins/modules/time-planning-pn/components/plannings/time-planning-actions/workday-entity/workday-entity-dialog.component.ts
+++ eform-client/src/app/plugins/modules/time-planning-pn/components/plannings/time-planning-actions/workday-entity/workday-entity-dialog.component.ts
@@ -1,5 +1,5 @@
 // src/app/plugins/modules/time-planning-pn/components/plannings/time-planning-actions/workday-entity/workday-entity-dialog.component.ts
-import {Component, EventEmitter, Inject, OnInit} from '@angular/core';
+import {Component, EventEmitter, Inject, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialogActions,
@@ -17,11 +17,12 @@ import {
   PlanningPrDayModel,
   PlanningPrDayUpdateModel
 } from '../../../../models';
-import {MtxGrid} from '@ng-matero/extensions/grid';
+import {MtxGrid, MtxGridColumn} from '@ng-matero/extensions/grid';
 import {TimePlanningPnPlanningsService} from '../../../../services';
 import * as R from 'ramda';
-import {MatFormField, MatLabel} from "@angular/material/form-field";
-import {MatInput} from "@angular/material/input";
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {NgxMaterialTimepickerModule} from "ngx-material-timepicker";
 
 @Component({
   selector: 'app-workday-entity-dialog',
@@ -40,7 +41,8 @@ import {MatInput} from "@angular/material/input";
     MtxGrid,
     MatFormField,
     MatInput,
-    MatLabel
+    MatLabel,
+    NgxMaterialTimepickerModule
   ],
   styleUrls: ['./workday-entity-dialog.component.scss']
 })
@@ -49,6 +51,9 @@ export class WorkdayEntityDialogComponent implements OnInit {
   workdayEntityUpdate: EventEmitter<PlanningPrDayUpdateModel> = new EventEmitter<PlanningPrDayUpdateModel>();
   enumKeys: string[];
   originalData: PlanningPrDayModel;
+  tableHeaders: MtxGridColumn[] = [];
+  @ViewChild('plannedColumnTemplate', { static: true }) plannedColumnTemplate!: TemplateRef<any>;
+  @ViewChild('actualColumnTemplate', { static: true }) actualColumnTemplate!: TemplateRef<any>;
   constructor(
     private planningsService: TimePlanningPnPlanningsService,
     @Inject(MAT_DIALOG_DATA) public data: PlanningPrDayModel,
@@ -62,25 +67,56 @@ export class WorkdayEntityDialogComponent implements OnInit {
     this.originalData = R.clone(this.data);
     this.enumKeys = Object.keys(TimePlanningMessagesEnum).filter(key => isNaN(Number(key)));
     this.data[this.enumKeys[this.data.message - 1]] = true;
+    //this.tableHeaders = [];
+
+    this.tableHeaders = [
+      { header: this.translateService.stream('Workday shift'), field: 'shift' },
+      {
+        cellTemplate: this.plannedColumnTemplate,
+        header: this.translateService.stream('Planned'),
+        field: 'plannedStart',
+        sortable: false,
+      },
+      {
+        cellTemplate: this.actualColumnTemplate,
+        header: this.translateService.stream('Actual'),
+        field: 'actualStart',
+        sortable: false,
+      },
+    ];
   }
 
-  columns = [
-    { header: this.translateService.stream('Workday shift'), field: 'shift' },
-    { header: this.translateService.stream('Planned'), field: 'planned' },
-    { header: this.translateService.stream('Actual'), field: 'actual' }
-  ];
+  // columns = [
+  //   { header: this.translateService.stream('Workday shift'), field: 'shift' },
+  //   { header: this.translateService.stream('Planned'), field: 'planned',
+  //     cellTemplate: this.plannedColumnTemplate, },
+  //   { header: this.translateService.stream('Actual'), field: 'actual',
+  //     cellTemplate: this.actualColumnTemplate, },
+  // ];
 
 
   shift2Data = {
     shift: this.translateService.instant('2nd'),
-    planned: this.data.plannedStartOfShift1 !== this.data.plannedEndOfShift1 && this.data.plannedEndOfShift2 !== 0 ? `${this.convertMinutesToTime(this.data.plannedStartOfShift2)} - ${this.convertMinutesToTime(this.data.plannedEndOfShift2)} / ${this.convertMinutesToTime(this.data.plannedBreakOfShift2)}` : '',
-    actual: this.data.start2StartedAt !== null ? `${this.datePipe.transform(this.data.start2StartedAt, 'HH:mm', 'UTC')} - ${this.data.stop2StoppedAt != null ? this.datePipe.transform(this.data.stop2StoppedAt, 'HH:mm', 'UTC') : '???'}` : ''
+    plannedStart: this.data.plannedStartOfShift2,
+    plannedEnd: this.data.plannedEndOfShift2,
+    plannedBreak: this.data.plannedBreakOfShift2,
+    actualStart: this.data.start2StartedAt,
+    actualEnd: this.data.stop2StoppedAt,
+    actualBreak: this.data.break2Shift,
+    //planned: this.data.plannedStartOfShift1 !== this.data.plannedEndOfShift1 && this.data.plannedEndOfShift2 !== 0 ? `${this.convertMinutesToTime(this.data.plannedStartOfShift2)} - ${this.convertMinutesToTime(this.data.plannedEndOfShift2)} / ${this.convertMinutesToTime(this.data.plannedBreakOfShift2)}` : '',
+    //actual: this.data.start2StartedAt !== null ? `${this.datePipe.transform(this.data.start2StartedAt, 'HH:mm', 'UTC')} - ${this.data.stop2StoppedAt != null ? this.datePipe.transform(this.data.stop2StoppedAt, 'HH:mm', 'UTC') : ''}` : ''
   };
 
   shift1Data = {
     shift: this.translateService.instant('1st'),
-    planned: this.data.plannedStartOfShift1 !== this.data.plannedEndOfShift1 ? `${this.convertMinutesToTime(this.data.plannedStartOfShift1)} - ${this.convertMinutesToTime(this.data.plannedEndOfShift1)} / ${this.convertMinutesToTime(this.data.plannedBreakOfShift1)}` : '',
-    actual: this.data.start1StartedAt !== null ? `${this.datePipe.transform(this.data.start1StartedAt, 'HH:mm', 'UTC')} - ${this.datePipe.transform(this.data.stop1StoppedAt, 'HH:mm', 'UTC')}` : ''
+    plannedStart: this.data.plannedStartOfShift1,
+    plannedEnd: this.data.plannedEndOfShift1,
+    plannedBreak: this.data.plannedBreakOfShift1,
+    actualStart: this.data.start1StartedAt,
+    actualEnd: this.data.stop1StoppedAt,
+    actualBreak: this.data.break1Shift,
+    //planned: this.data.plannedStartOfShift1 !== this.data.plannedEndOfShift1 ? `${this.convertMinutesToTime(this.data.plannedStartOfShift1)} - ${this.convertMinutesToTime(this.data.plannedEndOfShift1)} / ${this.convertMinutesToTime(this.data.plannedBreakOfShift1)}` : '',
+    //actual: this.data.start1StartedAt !== null ? `${this.datePipe.transform(this.data.start1StartedAt, 'HH:mm', 'UTC')} - ${this.datePipe.transform(this.data.stop1StoppedAt, 'HH:mm', 'UTC')}` : ''
   };
 
   shiftData = (this.data.isDoubleShift ? [this.shift1Data, this.shift2Data] : [this.shift1Data]);
