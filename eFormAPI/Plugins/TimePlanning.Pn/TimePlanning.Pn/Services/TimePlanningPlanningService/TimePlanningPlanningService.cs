6c224b84503626b10b2865dbd530b7b18c84a955
René Schultz Madsen <rm@microting.dk>
2025-04-03 07:36:58 +0200

    Adding changes to allowe for editing of all pauses in the first two shifts.

--- eFormAPI/Plugins/TimePlanning.Pn/TimePlanning.Pn/Services/TimePlanningPlanningService/TimePlanningPlanningService.cs
+++ eFormAPI/Plugins/TimePlanning.Pn/TimePlanning.Pn/Services/TimePlanningPlanningService/TimePlanningPlanningService.cs
@@ -1251,13 +1251,199 @@ public class TimePlanningPlanningService(
             planning.PlannedStartOfShift2 = model.PlannedStartOfShift2;
             planning.PlannedBreakOfShift2 = model.PlannedBreakOfShift2;
             planning.PlannedEndOfShift2 = model.PlannedEndOfShift2;
-            planning.PlanChangedByAdmin = true;
+            if (!planning.PlanChangedByAdmin)
+            {
+                var entry = dbContext.Entry(planning);
+                planning.PlanChangedByAdmin = entry.State == EntityState.Modified;
+            }
 
+            if (!assignedSite.UseDetailedPauseEditing)
+            {
+                planning.Pause1Id = model.Pause1Id ?? planning.Pause1Id;
+                planning.Pause2Id = model.Pause2Id ?? planning.Pause2Id;
+            }
+            else
+            {
+                planning.Shift1PauseNumber = 0;
+                planning.Pause1StartedAt = model.Pause1StartedAt;
+                planning.Pause1StoppedAt = model.Pause1StoppedAt;
+                if (planning.Pause1StartedAt != null && planning.Pause1StoppedAt != null)
+                {
+                    planning.Shift1PauseNumber = (int)((DateTime)planning.Pause1StoppedAt - (DateTime)planning.Pause1StartedAt).TotalMinutes;
+                }
+                planning.Pause2StartedAt = model.Pause2StartedAt;
+                planning.Pause2StoppedAt = model.Pause2StoppedAt;
+                if (planning.Pause2StartedAt != null && planning.Pause2StoppedAt != null)
+                {
+                    planning.Shift1PauseNumber += (int)((DateTime)planning.Pause2StoppedAt - (DateTime)planning.Pause2StartedAt).TotalMinutes;
+                }
+                planning.Pause10StartedAt = model.Pause10StartedAt;
+                planning.Pause10StoppedAt = model.Pause10StoppedAt;
+                if (planning.Pause10StartedAt != null && planning.Pause10StoppedAt != null)
+                {
+                    planning.Shift1PauseNumber += (int)((DateTime)planning.Pause10StoppedAt - (DateTime)planning.Pause10StartedAt).TotalMinutes;
+                }
+                
+                planning.Pause11StartedAt = model.Pause11StartedAt;
+                planning.Pause11StoppedAt = model.Pause11StoppedAt;
+                if (planning.Pause11StartedAt != null && planning.Pause11StoppedAt != null)
+                {
+                    planning.Shift1PauseNumber += (int)((DateTime)planning.Pause11StoppedAt - (DateTime)planning.Pause11StartedAt).TotalMinutes;
+                }
+                planning.Pause12StartedAt = model.Pause12StartedAt;
+                planning.Pause12StoppedAt = model.Pause12StoppedAt;
+                if (planning.Pause12StartedAt != null && planning.Pause12StoppedAt != null)
+                {
+                    planning.Shift1PauseNumber += (int)((DateTime)planning.Pause12StoppedAt - (DateTime)planning.Pause12StartedAt).TotalMinutes;
+                }
+                planning.Pause13StartedAt = model.Pause13StartedAt;
+                planning.Pause13StoppedAt = model.Pause13StoppedAt;
+                if (planning.Pause13StartedAt != null && planning.Pause13StoppedAt != null)
+                {
+                    planning.Shift1PauseNumber += (int)((DateTime)planning.Pause13StoppedAt - (DateTime)planning.Pause13StartedAt).TotalMinutes;
+                }
+                planning.Pause14StartedAt = model.Pause14StartedAt;
+                planning.Pause14StoppedAt = model.Pause14StoppedAt;
+                if (planning.Pause14StartedAt != null && planning.Pause14StoppedAt != null)
+                {
+                    planning.Shift1PauseNumber += (int)((DateTime)planning.Pause14StoppedAt - (DateTime)planning.Pause14StartedAt).TotalMinutes;
+                }
+                planning.Pause15StartedAt = model.Pause15StartedAt;
+                planning.Pause15StoppedAt = model.Pause15StoppedAt;
+                if (planning.Pause15StartedAt != null && planning.Pause15StoppedAt != null)
+                {
+                    planning.Shift1PauseNumber += (int)((DateTime)planning.Pause15StoppedAt - (DateTime)planning.Pause15StartedAt).TotalMinutes;
+                }
+                planning.Pause16StartedAt = model.Pause16StartedAt;
+                planning.Pause16StoppedAt = model.Pause16StoppedAt;
+                if (planning.Pause16StartedAt != null && planning.Pause16StoppedAt != null)
+                {
+                    planning.Shift1PauseNumber += (int)((DateTime)planning.Pause16StoppedAt - (DateTime)planning.Pause16StartedAt).TotalMinutes;
+                }
+                planning.Pause17StartedAt = model.Pause17StartedAt;
+                planning.Pause17StoppedAt = model.Pause17StoppedAt;
+                if (planning.Pause17StartedAt != null && planning.Pause17StoppedAt != null)
+                {
+                    planning.Shift1PauseNumber += (int)((DateTime)planning.Pause17StoppedAt - (DateTime)planning.Pause17StartedAt).TotalMinutes;
+                }
+                planning.Pause18StartedAt = model.Pause18StartedAt;
+                planning.Pause18StoppedAt = model.Pause18StoppedAt;
+                if (planning.Pause18StartedAt != null && planning.Pause18StoppedAt != null)
+                {
+                    planning.Shift1PauseNumber += (int)((DateTime)planning.Pause18StoppedAt - (DateTime)planning.Pause18StartedAt).TotalMinutes;
+                }
+                planning.Pause19StartedAt = model.Pause19StartedAt;
+                planning.Pause19StoppedAt = model.Pause19StoppedAt;
+                if (planning.Pause19StartedAt != null && planning.Pause19StoppedAt != null)
+                {
+                    planning.Shift1PauseNumber += (int)((DateTime)planning.Pause19StoppedAt - (DateTime)planning.Pause19StartedAt).TotalMinutes;
+                }
+                planning.Pause100StartedAt = model.Pause100StartedAt;
+                planning.Pause100StoppedAt = model.Pause100StoppedAt;
+                if (planning.Pause100StartedAt != null && planning.Pause100StoppedAt != null)
+                {
+                    planning.Shift1PauseNumber += (int)((DateTime)planning.Pause100StoppedAt - (DateTime)planning.Pause100StartedAt).TotalMinutes;
+                }
+                planning.Pause101StartedAt = model.Pause101StartedAt;
+                planning.Pause101StoppedAt = model.Pause101StoppedAt;
+                if (planning.Pause101StartedAt != null && planning.Pause101StoppedAt != null)
+                {
+                    planning.Shift1PauseNumber += (int)((DateTime)planning.Pause101StoppedAt - (DateTime)planning.Pause101StartedAt).TotalMinutes;
+                }
+                planning.Pause102StartedAt = model.Pause102StartedAt;
+                planning.Pause102StoppedAt = model.Pause102StoppedAt;
+                if (planning.Pause102StartedAt != null && planning.Pause102StoppedAt != null)
+                {
+                    planning.Shift1PauseNumber += (int)((DateTime)planning.Pause102StoppedAt - (DateTime)planning.Pause102StartedAt).TotalMinutes;
+                }
+
+                planning.Pause1Id = planning.Shift1PauseNumber / 5;
+                
+                planning.Shift2PauseNumber = 0;
+                planning.Pause20StartedAt = model.Pause20StartedAt;
+                planning.Pause20StoppedAt = model.Pause20StoppedAt;
+                if (planning.Pause20StartedAt != null && planning.Pause20StoppedAt != null)
+                {
+                    planning.Shift2PauseNumber += (int)((DateTime)planning.Pause20StoppedAt - (DateTime)planning.Pause20StartedAt).TotalMinutes;
+                }
+                planning.Pause21StartedAt = model.Pause21StartedAt;
+                planning.Pause21StoppedAt = model.Pause21StoppedAt;
+                if (planning.Pause21StartedAt != null && planning.Pause21StoppedAt != null)
+                {
+                    planning.Shift2PauseNumber += (int)((DateTime)planning.Pause21StoppedAt - (DateTime)planning.Pause21StartedAt).TotalMinutes;
+                }
+                planning.Pause22StartedAt = model.Pause22StartedAt;
+                planning.Pause22StoppedAt = model.Pause22StoppedAt;
+                if (planning.Pause22StartedAt != null && planning.Pause22StoppedAt != null)
+                {
+                    planning.Shift2PauseNumber += (int)((DateTime)planning.Pause22StoppedAt - (DateTime)planning.Pause22StartedAt).TotalMinutes;
+                }
+                planning.Pause23StartedAt = model.Pause23StartedAt;
+                planning.Pause23StoppedAt = model.Pause23StoppedAt;
+                if (planning.Pause23StartedAt != null && planning.Pause23StoppedAt != null)
+                {
+                    planning.Shift2PauseNumber += (int)((DateTime)planning.Pause23StoppedAt - (DateTime)planning.Pause23StartedAt).TotalMinutes;
+                }
+                planning.Pause24StartedAt = model.Pause24StartedAt;
+                planning.Pause24StoppedAt = model.Pause24StoppedAt;
+                if (planning.Pause24StartedAt != null && planning.Pause24StoppedAt != null)
+                {
+                    planning.Shift2PauseNumber += (int)((DateTime)planning.Pause24StoppedAt - (DateTime)planning.Pause24StartedAt).TotalMinutes;
+                }
+                planning.Pause25StartedAt = model.Pause25StartedAt;
+                planning.Pause25StoppedAt = model.Pause25StoppedAt;
+                if (planning.Pause25StartedAt != null && planning.Pause25StoppedAt != null)
+                {
+                    planning.Shift2PauseNumber += (int)((DateTime)planning.Pause25StoppedAt - (DateTime)planning.Pause25StartedAt).TotalMinutes;
+                }
+                planning.Pause26StartedAt = model.Pause26StartedAt;
+                planning.Pause26StoppedAt = model.Pause26StoppedAt;
+                if (planning.Pause26StartedAt != null && planning.Pause26StoppedAt != null)
+                {
+                    planning.Shift2PauseNumber += (int)((DateTime)planning.Pause26StoppedAt - (DateTime)planning.Pause26StartedAt).TotalMinutes;
+                }
+                planning.Pause27StartedAt = model.Pause27StartedAt;
+                planning.Pause27StoppedAt = model.Pause27StoppedAt;
+                if (planning.Pause27StartedAt != null && planning.Pause27StoppedAt != null)
+                {
+                    planning.Shift2PauseNumber += (int)((DateTime)planning.Pause27StoppedAt - (DateTime)planning.Pause27StartedAt).TotalMinutes;
+                }
+                planning.Pause28StartedAt = model.Pause28StartedAt;
+                planning.Pause28StoppedAt = model.Pause28StoppedAt;
+                if (planning.Pause28StartedAt != null && planning.Pause28StoppedAt != null)
+                {
+                    planning.Shift2PauseNumber += (int)((DateTime)planning.Pause28StoppedAt - (DateTime)planning.Pause28StartedAt).TotalMinutes;
+                }
+                planning.Pause29StartedAt = model.Pause29StartedAt;
+                planning.Pause29StoppedAt = model.Pause29StoppedAt;
+                if (planning.Pause29StartedAt != null && planning.Pause29StoppedAt != null)
+                {
+                    planning.Shift2PauseNumber += (int)((DateTime)planning.Pause29StoppedAt - (DateTime)planning.Pause29StartedAt).TotalMinutes;
+                }
+                planning.Pause200StartedAt = model.Pause200StartedAt;
+                planning.Pause200StoppedAt = model.Pause200StoppedAt;
+                if (planning.Pause200StartedAt != null && planning.Pause200StoppedAt != null)
+                {
+                    planning.Shift2PauseNumber += (int)((DateTime)planning.Pause200StoppedAt - (DateTime)planning.Pause200StartedAt).TotalMinutes;
+                }
+                planning.Pause201StartedAt = model.Pause201StartedAt;
+                planning.Pause201StoppedAt = model.Pause201StoppedAt;
+                if (planning.Pause201StartedAt != null && planning.Pause201StoppedAt != null)
+                {
+                    planning.Shift2PauseNumber += (int)((DateTime)planning.Pause201StoppedAt - (DateTime)planning.Pause201StartedAt).TotalMinutes;
+                }
+                planning.Pause202StartedAt = model.Pause202StartedAt;
+                planning.Pause202StoppedAt = model.Pause202StoppedAt;
+                if (planning.Pause202StartedAt != null && planning.Pause202StoppedAt != null)
+                {
+                    planning.Shift2PauseNumber += (int)((DateTime)planning.Pause202StoppedAt - (DateTime)planning.Pause202StartedAt).TotalMinutes;
+                }
+                planning.Pause2Id = planning.Shift2PauseNumber / 5;
+                // we need to calculate the pause id based on the start and stop times from all the pauses above
+            }
             planning.Start1Id = model.Start1Id ?? 0;
-            planning.Pause1Id = model.Pause1Id ?? planning.Pause1Id;
             planning.Stop1Id = model.Stop1Id ?? 0;
             planning.Start2Id = model.Start2Id ?? 0;
-            planning.Pause2Id = model.Pause2Id ?? planning.Pause2Id;
             planning.Stop2Id = model.Stop2Id ?? 0;
             planning.MessageId = model.Message;
 
