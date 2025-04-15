009cdeb99129e13a2f251c487fdab80c1b9910ea
René Schultz Madsen <rm@microting.dk>
2025-04-15 07:22:56 +0200

    Fixing the rounding of hours, since it should always be rounded down and the total minutes up.

--- eform-client/src/app/plugins/modules/time-planning-pn/components/plannings/time-plannings-table/time-plannings-table.component.ts
+++ eform-client/src/app/plugins/modules/time-planning-pn/components/plannings/time-plannings-table/time-plannings-table.component.ts
@@ -211,7 +211,7 @@ export class TimePlanningsTableComponent implements OnInit, OnChanges {
       hours = Math.abs(hours);
     }
     const totalMinutes = Math.round(hours * 60)
-    const hrs = Math.round(totalMinutes / 60);
+    const hrs = Math.floor(totalMinutes / 60);
     let mins = totalMinutes % 60;
     if (isNegative) {
       // return '${padZero(hrs)}:${padZero(60 - mins)}';
