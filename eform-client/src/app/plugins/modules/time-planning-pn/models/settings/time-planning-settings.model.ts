2835fc1df8d5ee9386d8dac673f94c298be98b41
René Schultz Madsen <rm@microting.dk>
2025-05-23 08:08:15 +0200

    Cleaning up setting spage.

--- eform-client/src/app/plugins/modules/time-planning-pn/models/settings/time-planning-settings.model.ts
+++ eform-client/src/app/plugins/modules/time-planning-pn/models/settings/time-planning-settings.model.ts
@@ -23,4 +23,6 @@ export class TimePlanningSettingsModel {
   fridayBreakMinutesUpperLimit: number;
   saturdayBreakMinutesUpperLimit: number;
   sundayBreakMinutesUpperLimit: number;
+  dayOfPayment: number;
+  showCalculationsAsNumber: boolean;
 }
