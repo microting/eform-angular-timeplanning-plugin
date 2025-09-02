358f59e3746e313cc1e20f9e00f8d43757690269
masudul-tikweb <masudul@tikweb.com>
2025-09-02 17:05:31 +0600

    added new cypress test case for the planhours and working hours adjustments

---
+++ eform-client/cypress/e2e/plugins/time-planning-pn/d/dashboard-edit-a.spec.cy.ts
@@ -0,0 +1,93 @@
+import loginPage from '../../../Login.page';
+
+describe('Dashboard edit values', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200');
+    loginPage.login();
+  });
+
+  it('should edit time registration in last week', () => {
+    cy.get('mat-nested-tree-node').contains('Timeregistrering').click();
+    cy.intercept('POST', '**/api/time-planning-pn/plannings/index').as('index-update');
+    cy.intercept('PUT', '**/api/time-planning-pn/plannings/*').as('saveWorkdayEntity');
+
+    cy.get('mat-tree-node').contains('Dashboard').click();
+    // cy.get('#backwards').click();
+    cy.wait('@index-update', { timeout: 60000 });
+    cy.get('#workingHoursSite').click();
+    cy.get('.ng-option').contains('ac ad').click();
+    cy.get('#cell0_0').click();
+
+    cy.get('#plannedStartOfShift1')
+      .closest('.flex-row')
+      .find('button mat-icon')
+      .contains('delete')
+      .click({ force: true });
+    cy.wait(500);
+
+    cy.get('#plannedEndOfShift1')
+      .closest('.flex-row')
+      .find('button mat-icon')
+      .contains('delete')
+      .click({ force: true });
+    cy.wait(500);
+
+    cy.get('#start1StartedAt')
+      .closest('.flex-row')
+      .find('button mat-icon')
+      .contains('delete')
+      .click({ force: true });
+    cy.wait(500);
+
+    cy.get('#stop1StoppedAt')
+      .closest('.flex-row')
+      .find('button mat-icon')
+      .contains('delete')
+      .click({ force: true });
+    cy.wait(500);
+
+    cy.get('#saveButton').click();
+    cy.wait('@saveWorkdayEntity', { timeout: 60000 });
+    cy.wait(1000);
+
+    // Registrar time
+    cy.get('#cell0_0').click();
+    cy.get('#plannedEndOfShift1').click();
+    cy.get('ngx-material-timepicker-face')
+      .contains('1')
+      .click({ force: true });
+    cy.get('ngx-material-timepicker-face')
+      .contains('00')
+      .click({ force: true });
+    cy.wait(1000);
+    cy.contains('button', /^Ok$/).click({ force: true });
+    cy.get('#plannedEndOfShift1').should('have.value', '01:00');
+    cy.get('#planHours').should('have.value', '1');
+    cy.get('#saveButton').click();
+    cy.wait('@saveWorkdayEntity', { timeout: 60000 });
+    cy.wait(1000);
+
+    cy.get('#cell0_0').click();
+    cy.get('#start1StartedAt').click();
+    cy.get('ngx-material-timepicker-face')
+      .contains('00')
+      .click({ force: true });
+    cy.get('ngx-material-timepicker-face')
+      .contains('00')
+      .click({ force: true });
+    cy.contains('button', /^Ok$/).click({ force: true });
+    cy.get('#stop1StoppedAt').click();
+    cy.get('ngx-material-timepicker-face')
+      .contains('1')
+      .click({ force: true });
+    cy.get('ngx-material-timepicker-face')
+      .contains('00')
+      .click({ force: true });
+
+    cy.contains('button', /^Ok$/).click({ force: true });
+    cy.wait(1000);
+    cy.get('#stop1StoppedAt').should('have.value', '01:00');
+    cy.get('#saveButton').click();
+    cy.wait('@saveWorkdayEntity', { timeout: 60000 });
+  });
+});
