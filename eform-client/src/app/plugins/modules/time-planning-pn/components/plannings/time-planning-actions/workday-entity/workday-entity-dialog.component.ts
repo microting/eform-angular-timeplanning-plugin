c2a139dc6c69e26c432e043687e098216d27ad8e
René Schultz Madsen <rm@microting.dk>
2025-09-12 12:45:51 +0200

    Fixing missing commentOffice save part.

--- eform-client/src/app/plugins/modules/time-planning-pn/components/plannings/time-planning-actions/workday-entity/workday-entity-dialog.component.ts
+++ eform-client/src/app/plugins/modules/time-planning-pn/components/plannings/time-planning-actions/workday-entity/workday-entity-dialog.component.ts
@@ -1124,7 +1124,6 @@ export class WorkdayEntityDialogComponent implements OnInit {
     // Rens paidOutFlex
     this.data.planningPrDayModels.paidOutFlex =
       this.data.planningPrDayModels.paidOutFlex === null ? 0 : this.data.planningPrDayModels.paidOutFlex;
-
     this.data.planningPrDayModels.commentOffice = this.workdayForm.get('commentOffice')?.value;
   }
 
