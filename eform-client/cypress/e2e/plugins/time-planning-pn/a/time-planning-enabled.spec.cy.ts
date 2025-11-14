9a7daee12525ed2c3612d172e84e71b7baf90d6f
René Schultz Madsen <rm@microting.dk>
2025-11-14 07:19:31 +0100

    Fixing plugin activation.

--- eform-client/cypress/e2e/plugins/time-planning-pn/a/time-planning-enabled.spec.cy.ts
+++ eform-client/cypress/e2e/plugins/time-planning-pn/a/time-planning-enabled.spec.cy.ts
@@ -10,14 +10,31 @@ describe('Enable Backend Config plugin', () => {
     //   .should('not.eq', 0) // we have plugins list
     //   .should('eq', 1); // we have only 1 plugin: time planning
   });
-  it('should enabled Time registration plugin', () => {
+
+    it('should enabled Time registration plugin', () => {
     const pluginName = 'Microting Time Planning Plugin';
     
     // Open action menu for the plugin
     cy.contains('.mat-mdc-row', pluginName).first().find('#actionMenu').click();
     cy.wait(500);
     
-    // Verify the plugin is enabled by checking the status button in the action menu
+    // Click the status button inside the menu to enable the plugin
+    cy.get('#plugin-status-button0').should('be.visible').click();
+    cy.wait(500);
+    
+    // Confirm activation in the modal if present
+    cy.get('body').then(($body) => {
+      if ($body.find('#pluginOKBtn').length > 0) {
+        cy.get('#pluginOKBtn').click();
+        cy.wait(100000); // Wait for plugin activation
+      }
+    });
+    
+    // Verify the plugin is enabled by checking the status
+    // Re-open the action menu to check the status
+    cy.contains('.mat-mdc-row', pluginName).first().find('#actionMenu').click();
+    cy.wait(500);
+    
     cy.get('#plugin-status-button0')
       .find('mat-icon')
       .should('contain.text', 'toggle_on'); // plugin is enabled
