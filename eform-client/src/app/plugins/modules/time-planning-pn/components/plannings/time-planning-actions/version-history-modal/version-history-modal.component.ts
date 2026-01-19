cc7f290eca0244e4786b0bff48b380c5d3f5f85e
René Schultz Madsen <rm@microting.com>
2026-01-19 17:53:31 +0100

    Adding more information to the versions modal.

--- eform-client/src/app/plugins/modules/time-planning-pn/components/plannings/time-planning-actions/version-history-modal/version-history-modal.component.ts
+++ eform-client/src/app/plugins/modules/time-planning-pn/components/plannings/time-planning-actions/version-history-modal/version-history-modal.component.ts
@@ -92,6 +92,7 @@ export class VersionHistoryModalComponent implements OnInit {
       day: '2-digit',
       hour: '2-digit',
       minute: '2-digit',
+      second: '2-digit',
       hour12: false
     });
   }
