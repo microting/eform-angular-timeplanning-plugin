8e506fb5f5ab83c620de37c752267fedb4fe1cbb
René Schultz Madsen <rm@microting.dk>
2026-03-06 19:06:22 +0100

    Fixing more tests

--- eform-client/src/app/plugins/modules/time-planning-pn/services/time-planning-pn-pay-rule-sets.service.spec.ts
+++ eform-client/src/app/plugins/modules/time-planning-pn/services/time-planning-pn-pay-rule-sets.service.spec.ts
@@ -55,7 +55,7 @@ describe('TimePlanningPnPayRuleSetsService', () => {
 
   describe('createPayRuleSet', () => {
     it('should call apiBaseService.post with correct parameters', (done) => {
-      const mockModel = { name: 'New RuleSet', payDayRules: [] };
+      const mockModel = { name: 'New RuleSet', payDayRules: [], payDayTypeRules: [] };
       const mockResponse = { success: true };
       mockApiBaseService.post.mockReturnValue(of(mockResponse as any));
 
